bf0d5bdd2444482d5f643071b8607f73b2ca4151
GodfreySam <godfreysam09@gmail.com>
2020-11-11 18:40:45 +0100

    Updates

---
+++ precache-manifest.fadfe50912068421150f610a0fe0e30e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39d915024ed11c71eafa75011bddccf",
+    "url": "/react-user-card/index.html"
+  },
+  {
+    "revision": "230aed95fde6c2affb60",
+    "url": "/react-user-card/static/css/main.12775694.chunk.css"
+  },
+  {
+    "revision": "ad1a4f0176c6d126b65c",
+    "url": "/react-user-card/static/js/2.9c54ae11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-user-card/static/js/2.9c54ae11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "230aed95fde6c2affb60",
+    "url": "/react-user-card/static/js/main.4c0b0595.chunk.js"
+  },
+  {
+    "revision": "b7dfd7aa301364fe60f0",
+    "url": "/react-user-card/static/js/runtime-main.9768d815.js"
+  }
+]);
